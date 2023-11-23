5afa1f89ba4591425e998a2d3ad0736422872abb
Bangkit Juang Raharjo <bangkitjuangraharjo@gmail.com>
2023-11-23 10:55:24 +0700

    update pmebelajaran

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import mustache from 'mustache';
+import fs from 'fs/promises'
+
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  let indexTemplate = await fs.readFile('./views/index.mustache').then(data => data.toString())
+  mustache.parse(indexTemplate)
+
+  let webview = mustache.render(indexTemplate, {
+    title: 'Express'
+  })
+
+  res.send(webview)
 });
 
 export default router;
